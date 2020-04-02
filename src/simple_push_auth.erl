8d3d3f4b96beb12b9afe8e3c4e63618940a3c186
Niclas Axelsson <niclas@burbas.se>
2020-04-02 08:36:52 +0200

    Remove unused start.sh and fix a problem with the auth-handler

--- src/simple_push_auth.erl
+++ src/simple_push_auth.erl
@@ -6,7 +6,7 @@ basic_auth(Req) ->
     %% Check if this user is authenticated
     case cowboy_req:parse_header(<<"authorization">>, Req) of
         {basic, <<"test">>, <<"test">>} ->
-            {ok, true};
+            true;
         _ ->
             false
     end.
