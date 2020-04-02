c06ca422cc3b16a52ce1aa2b1579d7c179f7912d
Daniel Widgren <daniel.widgren@gmail.com>
2020-04-02 08:51:41 +0200

    changed keys from atom to binary

--- src/controllers/simple_push_account_controller.erl
+++ src/controllers/simple_push_account_controller.erl
@@ -8,9 +8,8 @@
 apns(#{method := <<"POST">>} = Req) ->
     {ok, Data, _Req0} = cowboy_req:read_body(Req),
     JSON = json:decode(Data, [maps]),
-    Account = #account{id = maps:get(id, JSON),
-                       team_id = maps:get(team_id, JSON),
-                       key_id = maps:get(key_id, JSON),
-                       p8_key = maps:get(p8_key, JSON)},
+    Account = #account{team_id = maps:get(<<"team_id">>, JSON),
+                       key_id = maps:get(<<"key_id">>, JSON),
+                       p8_key = maps:get(<<"p8_key">>, JSON)},
     {ok, Account1} = simple_push_db:create_account(Account),
     {json, #{id => Account1#account.id}}.
