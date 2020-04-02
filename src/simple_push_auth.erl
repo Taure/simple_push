3a3c2a3ea77e5bac47f08d02b75997889e215ee9
Daniel Widgren <daniel.widgren@gmail.com>
2020-04-02 07:58:58 +0200

    added fixes

--- src/simple_push_auth.erl
+++ src/simple_push_auth.erl
@@ -1,11 +1,12 @@
 -module(simple_push_auth).
 
+-export([basic_auth/1]).
 
 basic_auth(Req) ->
     %% Check if this user is authenticated
     case cowboy_req:parse_header(<<"authorization">>, Req) of
         {basic, <<"test">>, <<"test">>} ->
-            true;
+            {ok, true};
         _ ->
             false
     end.
